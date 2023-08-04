4241980a724a462e4bc70e18466ac023ed79016c
INoorik <ajnurabuzarov8@gmail.com>
2023-08-04 11:42:56 +0300

    Player class added

---
+++ rplugin/python3/player.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+class Player:
+    def __init__(self, nvim, window, video):
+        self.nvim = nvim
+        self.window = window
+        self.video = video
+    def play(self):
+        pass
