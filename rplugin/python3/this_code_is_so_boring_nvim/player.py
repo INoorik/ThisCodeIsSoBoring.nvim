a83088e47c5871be660c7f9572c681ae625a0109
INoorik <ajnurabuzarov8@gmail.com>
2023-08-04 20:09:11 +0300

    missed renderer call added

--- rplugin/python3/this_code_is_so_boring_nvim/player.py
+++ rplugin/python3/this_code_is_so_boring_nvim/player.py
@@ -1,7 +1,7 @@
 from PIL import Image
 from PIL import ImageOps
 import cv2
-from image_render import ImageRenderer
+from .image_render import ImageRenderer
 from time import sleep
 
 class Player:
@@ -14,7 +14,7 @@ class Player:
         success, image = self.video.read()
         image = Image.fromarray(image)
         while success and self.window.valid:
-            self.window.buffer[:] = self._resize_image_to_fit_window(image)
+            self.window.buffer[:] = self.renderer.render(self._resize_image_to_fit_window(image))
             sampling_period = 1/self.video.get(cv2.CAP_PROP_FPS)
             sleep(sampling_period)
     def _resize_image_to_fit_window(self, image):
