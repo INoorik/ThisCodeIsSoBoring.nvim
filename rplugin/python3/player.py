d6c2c146d0a452c1df735c2f2c798f27d55af211
INoorik <ajnurabuzarov8@gmail.com>
2023-08-04 19:49:11 +0300

    files from python3 dir deleted

--- rplugin/python3/player.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-
-class Player:
-    def __init__(self, nvim, window, video):
-        self.nvim = nvim
-        self.window = window
-        self.video = video
-    def play(self):
-        pass
