f181c5e3e134b888ce3dd3cf522cb69f7aa707f0
INoorik <ajnurabuzarov8@gmail.com>
2023-08-09 22:05:49 +0300

    commands made async

--- rplugin/python3/this_code_is_so_boring_nvim/__init__.py
+++ rplugin/python3/this_code_is_so_boring_nvim/__init__.py
@@ -9,15 +9,18 @@ class Test:
     def __init__(self, nvim):
         self.default_video = "https://ia801901.us.archive.org/14/items/xbdv-9403/XBDV9403.mp4"
         self.nvim = nvim
-    @pynvim.command('ThisCodeIsSoBoring', range='', nargs='?', sync=True)
+    @pynvim.command('ThisCodeIsSoBoring', range='', nargs='?', sync=False)
     def play(self, args, range):
         if len(args) == 0:
             video = cv2.VideoCapture(self._get_absolute_path(self.default_video))
         else:
             video = cv2.VideoCapture(self._get_absolute_path(args[0]))
+        if not video.isOpened():
+            print("Unable to open video")
+            return
         player = Player(self.nvim, self.nvim.current.window, video)
         self.nvim.async_call(player.play)
-    @pynvim.command('SetThisCodeIsSoBoringVideo', range='', nargs=1, sync=True)
+    @pynvim.command('SetThisCodeIsSoBoringVideo', range='', nargs=1, sync=False)
     def set_default(self, args, range):
         self.default_video = args[0]
     def _get_absolute_path(self, path):
